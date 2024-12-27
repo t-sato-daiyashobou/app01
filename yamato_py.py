b8ebe851a2fbe32d6dab593335e00c1a5b9ad9ce
t-sato-daiyashobou <t-sato@daiyashobou.com>
2024-12-27 11:33:07 +0900

    202412271133

--- yamato_py.py
+++ yamato_py.py
@@ -3,24 +3,24 @@ import requests
 import urllib3
 from bs4 import BeautifulSoup
 
-
 if __name__ == "__main__":
     ctx = ssl.create_default_context()
-    ctx.set_ciphers('AESGCM')
-    
+    ctx.set_ciphers("AESGCM")
+
     adapter = requests.adapters.HTTPAdapter()
     adapter.poolmanager = urllib3.PoolManager(
-        ssl_version=ssl.PROTOCOL_TLSv1_2,
-        ssl_context=ctx
+        ssl_version=ssl.PROTOCOL_TLSv1_2, ssl_context=ctx
     )
 
     session = requests.Session()
     session.verify = True
-    session.mount('https://', adapter)
-    
+    session.mount("https://", adapter)
 
     url = "https://inter-consistent2.kuronekoyamato.co.jp/consistent2/cts"
-    payload = r"""<?xml version="1.0" encoding="UTF-8"?>    
+    
+    tracking_number = "437111342076" 
+    
+    payload = f"""<?xml version="1.0" encoding="UTF-8"?>    
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
         <soapenv:Body>
             <ns1:provideXMLTraceService xmlns:ns1="urn:ClientReception">
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                             </検索オプション>
                             <検索条件>
                                 <伝票番号検索>
-                                    <伝票番号>437111342076</伝票番号>
+                                    <伝票番号>{tracking_number}</伝票番号>
                                 </伝票番号検索>
                             </検索条件>
                         </問合せ要求>
@@ -53,7 +53,8 @@ if __name__ == "__main__":
     }
 
     with session.post(url, headers=headers, data=payload) as r:
-        data = BeautifulSoup(BeautifulSoup(r.text, "xml").text, "xml")
+        soap_response = BeautifulSoup(r.text, "xml")
+        data = BeautifulSoup(soap_response.text, "xml")
 
-        for i in data.select("検索キータイトル2"):
+        for i in data.select("検索キータイトル1,検索キータイトル2"):
             print(i.prettify())
